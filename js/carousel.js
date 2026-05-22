372f22db32bf3d0a926e33147d3f48506b795333
PradoRuhan <ruhan-prado@hotmail.com>
2026-05-22 16:06:11 -0300

    Montagem do carrosel aditando o javascript e o css

--- js/carousel.js
+++ js/carousel.js
@@ -1,32 +1,38 @@
-
-
 //carousel
 
 //Array storage class
 let carouselArr = [];
 
-
 //class Carousel
 class Carousel {
 
-    
+    constructor(imagem,texto,link){
+        this.imagem = imagem;
+        this.texto = texto;
+        this.link = link;
+    }
       
     static Start(arr){
-        if(arr){
-
-            if(arr.length > 0){
-                Carousel._sequence = 0;
-                Carousel._size = arr.length;
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
-            }
-            
-        } else {
-            throw "Method Start need a Array Variable.";
+        if(arr&&arr.length > 0){
+            Carousel._arr = arr;
+            Carousel._index = 0;
+            Carousel.Next(); //start
+            Carousel._interval = setInterval(function(){ Carousel.Next(); },3000);
         }
     }
 
     static Next(){
-        
+        const carouselDiv = document.getElementById("carousel");
+        const textoDiv = document.getElementById("carousel-title");
+        const atual = Carousel._arr[Carousel._index];
+
+        carouselDiv.innerHTML = `<img src="img/${atual.imagem}">`;
+        textoDiv.innerHTML = `<a href="${atual.link}">${atual.texto}</a>`;
+
+        Carousel._index = (Carousel._index+1) % Carousel._arr.length;
+        Carousel._index++;
+        if(Carousel._index >= Carousel._arr.length){
+            Carousel._index = 0;
+        }
     }
 };
