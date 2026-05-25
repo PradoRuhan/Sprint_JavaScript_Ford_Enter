5bfba19b37f1b21b95576fc4cefe009f3bff79ca
PradoRuhan <ruhan-prado@hotmail.com>
2026-05-25 11:18:32 -0300

    Atualizações da aba de contatos e criação do botão do carrossel

--- js/carousel.js
+++ js/carousel.js
@@ -17,7 +17,38 @@ class Carousel {
             Carousel._arr = arr;
             Carousel._index = 0;
             Carousel.Next(); //start
-            Carousel._interval = setInterval(function(){ Carousel.Next(); },3000);
+            Carousel._ResetInterval();
+            Carousel.CreateButtons();
+        }
+    }
+
+    static ResetInterval(){
+        if (Carousel._interval){
+            clearInterval(Carousel._interval);
+        }
+        Carousel._interval = setInterval(function(){
+            Carousel.Next();
+        }, 3000);
+    }
+
+    static ManualNext(){
+        Carousel.Next();
+        Carousel._ResetInterval();
+    }
+
+    static CreateButton(){
+        if (!document.getElementById("btn-next-carousel")){
+            const btn = document.createElement("button");
+            btn.id = "btn-next-carousel";
+            btn.innerText = "Próximo >";
+            btn.className = "btn-carousel";
+            btn.onclick = function(){
+                Carousel.ManualNext();
+            };
+            const textoDiv = document.getElementById("carousel-title");
+            if (textoDiv){
+                textoDiv.after(btn);
+            }
         }
     }
 
@@ -26,13 +57,11 @@ class Carousel {
         const textoDiv = document.getElementById("carousel-title");
         const atual = Carousel._arr[Carousel._index];
 
-        carouselDiv.innerHTML = `<img src="img/${atual.imagem}">`;
-        textoDiv.innerHTML = `<a href="${atual.link}">${atual.texto}</a>`;
+        if (carouselDiv && textoDiv && atual){
+            carouselDiv.innerHTML = `<img src="img/${atual.imagem}">`;
+            textoDiv.innerHTML = `<a href="${atual.link}">${atual.texto}</a>`;
+        }
 
         Carousel._index = (Carousel._index+1) % Carousel._arr.length;
-        Carousel._index++;
-        if(Carousel._index >= Carousel._arr.length){
-            Carousel._index = 0;
-        }
     }
 };
